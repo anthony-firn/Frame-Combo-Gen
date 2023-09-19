8ca58245c7457acceb6ae9a23f6d8de7e6ad7c62
Anthony Firn <anthonyfirn@illi.ac>
2023-09-18 18:36:45 -0700

    Added frame extraction, static scene detection, frame combination, and dataset organization functionalities

--- frame_combo_gen.py
+++ frame_combo_gen.py
@@ -49,24 +49,39 @@ def process_scene(scene, x, y):
     # Extract frames based on x and y
     frames = extract_frames(scene, x, y)
     
+    # Check if scene is static
+    if is_static_scene(frames):
+        return
+    
     # Combine frames into single images
     combined_image = combine_frames(frames)
     
     # Organize combined images into dataset folders
     organize_images(combined_image, x, y)
 
-# Placeholder functions for the new functionalities
+# Extract frames from a scene based on x and y values
 def extract_frames(scene, x, y):
     # Implement frame extraction logic based on x and y values
-    pass
+    # Placeholder logic
+    extracted_frames = []
+    for i in range(0, len(scene), x):
+        extracted_frames.append(scene[i:i+y])
+    return extracted_frames
 
+# Combine frames into a single image
 def combine_frames(frames):
     # Implement logic to combine frames into a single image
-    pass
+    # Placeholder logic
+    return np.hstack(frames)
 
+# Organize combined images into dataset folders
 def organize_images(image, x, y):
     # Implement logic to organize images into dataset folders
-    pass
+    # Placeholder logic
+    folder_name = f"x_{x}_y_{y}"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    cv2.imwrite(os.path.join(folder_name, f"combined_{x}_{y}.jpg"), image)
 
 # Extract metadata from video
 def extract_metadata(video_path):
@@ -104,8 +119,7 @@ def main():
 
         scenes = extract_scenes(video_path)
         for scene in scenes:
-            if not is_static_scene(scene):
-                process_scene(scene, x=2, y=2)  # Example values
+            process_scene(scene, x=2, y=2)  # Example values
 
         metadata_list.append(metadata)
 
